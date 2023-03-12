0e9dc2b20a0234737a7a703fc9e8fb47a2a5dd4d
Pierre Hagelberg <pierre.hagelberg@gmail.com>
2023-03-12 19:17:02 +0100

    update front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,50 @@
+import Card from "@/components/card/card";
 import styles from './page.module.css'
-import { ComparisonContainer, ComparisonItem } from '@/components/comparison'
+
+async function getData() {
+  const res = await fetch('https://fruityvice.com/api/fruit/all')
+  if (!res.ok) {
+    throw new Error('failed to fetch data')
+  }
+  return res.json()
+}
+
+interface IFruit {
+  genus?: string;
+  name?: string;
+  id?: number;
+  family?: string;
+  order?: string;
+  nutritions?: {
+    carbohydrates?: number;
+    protein?: number;
+    fat?: number;
+    calories?: number;
+    sugar?: number;
+  }
+}
 
 
-export default function Home() {
+const Home = async () => {
+  const data: IFruit[] = await getData()
+  console.log(data[0].name)
   return (
     <main>
       <h1>Fruit Comparison</h1>
-      <section>
-        <ComparisonContainer>
-          <ComparisonItem>
-            One
-          </ComparisonItem>
-          <ComparisonItem>
-            Two
-          </ComparisonItem>
-        </ComparisonContainer>
+      <section className={styles.container}>
+
+        {data && data.map((props, i) => (
+          <Card key={i} {...props} />
+        )
+        )}
+
+        <p> End of Data</p>
       </section>
     </main>
   )
 }
+
+export default Home
+export type { IFruit }
+
+
